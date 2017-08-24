d02e69da19c79d1a28ad287eba82bc29ba00f368
Ali Zaidi <msazaidi@gmail.com>
2017-08-23 22:45:30 -0700

    Changed location of tether in js assets

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,7 +13,7 @@
 //= require jquery
 //= require jquery_ujs
 //= require turbolinks
+//= require tether
 //= require bootstrap-sprockets
 //= require bootstrap-datepicker
-//= require tether
 //= require_tree .
